c8a0d53eb8972a0cc608d9a82307cae694306ff8
Sachinmahajana <sachinmahajan782@gmail.com>
2023-04-27 00:11:36 +0530

    [Sachin] Adding Dictionary

--- Collection/Program.cs
+++ Collection/Program.cs
@@ -5,6 +5,38 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the collection program");
+            Dictionary<int ,string> student = new Dictionary<int ,string>();
+            //Adding the element in Dictionary
+            student.Add(1, "Aman");
+            student.Add(2, "Mohan");
+            student.Add(3, "Rohan");
+            student.Add(4, "Sohan");
+            
+            foreach(KeyValuePair<int,string> kv in student)
+            {
+                Console.WriteLine(kv.Key + " " + kv.Value);
+            }
+            //Find the student entry
+            Console.WriteLine("Enter the Key to search for Names");
+            int i = Convert.ToInt32(Console.ReadLine());
+            if (student.ContainsKey(i))
+            {
+                Console.WriteLine("Student name found is: "+ student[i]);
+            }
+            else
+            {
+                Console.WriteLine("Student entry not found");
+            }
+            //Remove element
+            student.Remove(i);
+            Console.WriteLine("After removing the entry");
+            foreach(KeyValuePair<int,string> kv in student)
+            {
+                Console.WriteLine(kv.Key + " " + kv.Value);
+            }
+
+
+
         }
     }
 }
