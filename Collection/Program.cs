8e871b1a4da85c6f39f38cb09015d0b69be1b285
Sachinmahajana <sachinmahajan782@gmail.com>
2023-04-26 22:32:19 +0530

    [Sachin] Adding ArrayList

--- Collection/Program.cs
+++ Collection/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace Collection
+﻿using System.Collections;
+
+namespace Collection
 {
     internal class Program
     {
-        static void Main(string[] args)
+       public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the collection program");
+            //Adding the arraylist element
+            ArrayList myList = new ArrayList();
+            myList.Add(10);
+            myList.Add(20);
+            myList.Add(30);
+            myList.Add(40);
+            Console.WriteLine(myList.Capacity);
+            myList.Add(60);
+            myList.Add(70);
+            //Shows the capacity
+            Console.WriteLine(myList.Capacity);
+            foreach(int i in myList)
+            {
+                Console.WriteLine(i);
+            }
+            //Insert the element
+            myList.Insert(2, 25);
+            myList.Insert(3, 39);
+            foreach (int i in myList)
+            {
+                Console.WriteLine(i);
+            }
+            //remove element
+            myList.Remove(40);
+            myList.RemoveAt(5);
+            foreach(int i in myList)
+            {
+                Console.WriteLine(i);
+            }
+
+
         }
     }
 }
